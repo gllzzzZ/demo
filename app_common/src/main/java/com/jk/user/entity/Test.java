f299d01e8a09b9ae201b70b7adc3691d0ce55495
gllzzzZ <1315681598@qq.com>
2018-04-10 10:01:46 +0800

    加了一个类

---
+++ app_common/src/main/java/com/jk/user/entity/Test.java
@@ -0,0 +1,9 @@
+package com.jk.user.entity;
+
+import java.io.Serializable;
+
+public class Test implements Serializable {
+
+    private static final long serialVersionUID = -7610615311933670768L;
+
+}
